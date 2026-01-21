89eb9b9b99afe6e0308a4f92fdb24d2edfc2058c
Robert Skaug <robert.skaug@klp.no>
2026-01-21 11:41:22 +0100

    Bump SW cache to v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "befaring-pwa-v1";
+const CACHE = "befaring-pwa-v2";
 const ASSETS = [
   "./",
   "./index.html",
@@ -10,21 +10,17 @@ const ASSETS = [
 ];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
-  );
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
 });
 
 self.addEventListener("activate", (event) => {
+  self.clients.claim();
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
   );
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
 });
